5c135c634923daf928c4dc2135694cbe8dbc7711
tiliti <tibor.tiliczki@gmail.com>
2024-01-16 06:41:58 +0100

    modified

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
+
+/* File: csaladfa
+* Author: Tiliczki Tibor
+* Copyright: 2024, Tiliczki Tibor
+* Group: Szoft II/I/E
+* Date: 2024-01-15
+* Github: https://github.com/tilitihub
+* Licenc: GNU GPL
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details:
+* http://www.gnu.org/licenses/gpl.html
+*/
+
 const express = require('express');
 const sqlite3 = require('sqlite3');
 const path = require('path');
